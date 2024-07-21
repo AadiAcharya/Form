7b08faca72acb85b1a1280ea284c4bda4d35406e
Lawar james chaudhary <lawarjames67929@gmail.com>
2024-07-21 13:21:29 +0545

    katti change garna pareko yrr

--- src/Component/FirstPage.jsx
+++ src/Component/FirstPage.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const FirstPage = () => {
   return (
-    <div>
-       
-    <div className="font-royal bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 flex-col  h-[100vh] w-[100vw] flex text-3xl text-sky-300 justify-center items-center "> 
-    <h1 className='absolute top-20 text-5xl text-white'>Digital Launchpad Nepal</h1>
-      <div className='flex flex-col py-10 justify-center items-center'>
-      <div>     
-         <h1 className='pb-20 text-white'>Join our journey</h1>
-</div>
-      <div className='pb-20 text-white  top-36 '>
-    
-    <p className='text-center'>Enhancing the IT road-map <br /> for innovation & growth.</p>
+    <div className="font-royal bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 flex flex-col h-screen w-screen text-sky-300 justify-center items-center p-4 md:p-8 lg:p-12">
+      <h1 className='text-3xl md:text-4xl lg:text-5xl text-white mb-4'>
+        Digital Launchpad Nepal
+      </h1>
+      <div className='flex flex-col justify-center items-center text-center'>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl text-white mb-4'>
+          Join our journey
+        </h2>
+        <p className='text-base md:text-lg lg:text-xl text-white mb-6'>
+          Enhancing the IT road-map <br /> for innovation & growth.
+        </p>
+      </div>
+      <NavLink to='/form'>
+        <button className='bg-yellow-300 text-blue-500 text-lg md:text-xl lg:text-2xl rounded-full p-4 md:p-6 lg:p-8 hover:bg-yellow-400 hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-yellow-300'>
+          Enroll Now
+        </button>
+      </NavLink>
     </div>
-    </div>
-    <div className='flex justify-center items-center'>
-        <NavLink to='/form'>
-          <button className=' p-10 rounded-[50px]  bg-yellow-300 text-blue-500 text-3xl hover:bg-yellow-500  hover:text-blue-900 focus:bg-sky-600'>Enroll Now</button>
-        </NavLink>
-    </div>
-     </div> 
+  );
+};
 
-    </div>
-  )
-}
-
-export default FirstPage
+export default FirstPage;
